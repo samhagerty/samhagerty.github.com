3f7f22d8bf884c16d38987f468495b7592a5fdf9
Sam Hagerty <s.hagerty@dfsales.co.uk>
2013-06-04 21:26:03 +0100

    revert

---
+++ _posts/2013-04-28-welcome-all.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "Hagerty goes live!"
+description: "The biggest news since sliced bread."
+published: true
+---
+It is official everyone, let it be known that from here on out Hagerty is running the world… well sort of. He has his own website, but I guess everyone has to start somewhere right?
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,14 @@
-// On load, show the bouncer
-if ($('html').hasClass('csstransitions') && $('html').hasClass('borderradius') && !$('html').hasClass('no-js')) {
-	if ($('header#404').length > 0) 
-		show404Bouncer();
-	else
-		showBouncer();
-} else {
-	$('html').addClass('no-js');
-}
-
-if ($('.video-wrapper').length > 0) {
-	// Listen for page size and write video size appropriately
-	resizeVideoTo80Percent();
-	$(window).bind("resize", resizeVideoTo80Percent);
-}
-
-function resizeVideoTo80Percent() {
-	if ($(window).width() < 720) {
-		var videoWidth = $(window).width() * 0.8;
-		var videoHeight = $(window).width() * 0.50;
-		$('.video-wrapper').width(videoWidth);
-		$('.video-wrapper iframe').width(videoWidth).height(videoHeight);
+(function(){
+	// On load, show the bouncer
+	if ($('html').hasClass('csstransitions') && $('html').hasClass('borderradius') && !$('html').hasClass('no-js')) {
+		if ($('header#404').length > 0) 
+			show404Bouncer();
+		else
+			showBouncer();
 	} else {
-		$('.video-wrapper').width(720);
-		$('.video-wrapper iframe').width(720).height($('.video-wrapper iframe').attr('data-height'));
+		$('html').addClass('no-js');
 	}
-}
+}());
 
 function showBouncer() {
 	$('.bouncer-container a').append($('#bouncer-template').html());
